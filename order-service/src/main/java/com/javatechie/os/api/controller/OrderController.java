e1b170a719a7ecbfecad26df858e64906eed47b6
IntakhabAnsari <intakhab318@gmail.com>
2025-09-25 19:20:14 +0530

    1st lecture of microservice

--- order-service/src/main/java/com/javatechie/os/api/controller/OrderController.java
+++ order-service/src/main/java/com/javatechie/os/api/controller/OrderController.java
@@ -1,5 +1,23 @@
 package com.javatechie.os.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.javatechie.os.api.entity.Order;
+import com.javatechie.os.api.service.OrderService;
+
+@RestController
+@RequestMapping("/order")
 public class OrderController {
 
-}
+	@Autowired
+	private OrderService service;
+	
+	@PostMapping("/bookOrder")
+	public Order bookOrder(@RequestBody Order order) {
+		return service.saveOrder(order);
+	}
+}                                                                 
